b5778b1930aed21b746e81521a9857a455bec23c
Ichai Wizman <ichaiwizm@gmail.com>
2023-04-30 12:05:44 +0200

    J'ai ajouté une transition aux images de l'acceuil et ajouté une balise pour suivre le flux du site

--- src/pages/Acceuil/Acceuil.js
+++ src/pages/Acceuil/Acceuil.js
@@ -1,38 +1,35 @@
 import React from "react";
 import NavBarAcceuil from "../../components/NavBarAcceuil/NavBarAcceuil";
 import "./Acceuil.css";
-import imgprj1 from "../../../src/assets/RobotGentil.jpeg"
-import imageprojet2 from "../../../src/assets/RobotFlippant.jpeg"
+import imgprj1 from "../../../src/assets/RobotGentil.jpeg";
+import imageprojet2 from "../../../src/assets/RobotFlippant.jpeg";
 import { Link } from "react-router-dom";
 
 const Acceuil = () => {
-
-return (
+  return (
     <div className="all-acceuil">
       <NavBarAcceuil />
       <div className="containerStyle">
-      <div className="cardStyle">
-        <div className="titleContainerStyle">
-          <h2 className="titleStyle">Mon projet numéro 1</h2>
+        <div className="cardStyle">
+          <div className="titleContainerStyle">
+            <h2 className="titleStyle">Mon projet numéro 1</h2>
+          </div>
+          <img src={imgprj1} alt="Projet 1" className="imageStyle" />
+          <Link to="/ProjetN1" className="linkStyle">
+            Projet n°1
+          </Link>
         </div>
-        <img src={imgprj1} alt="Projet 1" className="imageStyle" />
-            <Link to="/ProjetN1" className="linkStyle">Projet n°1</Link>
-      </div>
-      <div className="cardStyle">
-        <div className="titleContainerStyle">
-          <h2 className="titleStyle">Mon projet numéro 2</h2>
+        <div className="cardStyle">
+          <div className="titleContainerStyle">
+            <h2 className="titleStyle">Mon projet numéro 2</h2>
+          </div>
+          <img src={imageprojet2} alt="Projet 2" className="imageStyle" />
+          <Link to="/Counter" className="linkStyle">
+            Projet n°2
+          </Link>
         </div>
-        <img
-          src={imageprojet2}
-          alt="Projet 2"
-          className="imageStyle"
-        />
-        <Link to="/Counter" className="linkStyle">Projet n°2</Link>
       </div>
-      {/* Vous pouvez ajouter encore plus de projets ici */}
-    </div>
     </div>
   );
-}
+};
 export default Acceuil;
-  
